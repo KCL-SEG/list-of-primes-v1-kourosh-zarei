5295ecddbeabea3fc6c4c8c96f45309f4b7db9f1
kourosh-zarei <126338174+kourosh-zarei@users.noreply.github.com>
2023-09-25 14:59:54 +0100

    feat: is_prime func & prime count fully working

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
+
+def is_prime(num: int):
+    for divisor in range(2, math.ceil(num / 2) + 1):
+        if num % divisor == 0:
+            return False
+    return True
+
+
 def primes(number_of_primes):
-    list = []
-    return list
+    _list = []
+    current_num = 1
+    while len(_list) < number_of_primes:
+        current_num += 1
+        if is_prime(current_num):
+            _list.append(current_num)
+    return _list
